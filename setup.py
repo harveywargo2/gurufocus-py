8755901261600af4dc713b4c0994c1679257c9da
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2023-11-08 13:44:15 -0500

    fix pypi error

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(
     name='gurupy',
     version='0.1',
     author='Harvey Wargo',
-    packages=find_packages('gurupy'),
-    package_dir={'': 'gurupy'},
+    packages=find_packages(),
     url='https://github.com/harveywargo2/guru-wrangler-py',
     keywords='stock financials dividends gurufocus',
     install_requires=[
