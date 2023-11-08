055c3f48021a782bc617c2f62687d58f167d60ad
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2023-11-08 13:54:12 -0500

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gurupy',
-    version='0.1',
+    version='0.2',
     author='Harvey Wargo',
     packages=find_packages(),
     url='https://github.com/harveywargo2/guru-wrangler-py',
