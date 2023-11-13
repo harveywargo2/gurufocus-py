21804e0587f77657a6c4ca56ac33162d65755bb6
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2023-11-13 15:49:21 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 import setuptools
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name='gurupy',
-    version='0.3',
+    version='0.4',
     author='Harvey Wargo',
-    packages=setuptools.find_packages(include=['gurupy' , 'gurupy.*']),
     description=('Python Data Wrangler Modules for GuruFocus Stock API'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(include=['gurupy', 'gurupy.*']),
     url='https://github.com/harveywargo2/guru-wrangler-py',
     keywords='stock financials dividends gurufocus gurupy 10K',
     install_requires=[
         'requests',
-        'pandas'
+        'pandas',
+        'os'
     ]
 
 )
